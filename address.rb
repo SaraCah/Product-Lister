1353b3b39d2373429d9b1c45d57bb4f45fb0a221
Sara Cave <saracaveg@gmail.com>
2019-09-05 16:47:40 +1000

    wanted to format the input for the address and have a long version or short version in case ppl dont have more than one address line

--- address.rb
+++ address.rb
@@ -10,4 +10,21 @@ class Address
         @state = state
         @postal_code = postal_code
     end
+    def to_s(format = 'short') #format of how it's inputed
+        address = '' 
+        case format
+        when 'long' #input for  lonf address with 2 streets because i dont want empty space
+            address += street_1 + "\n" #puts space so formatted nicely
+            address += street_2 + "\n" unless street_2.nil? || street_2.empty? 
+            address += "#{city}, #{state}, #{postal_code}" # put
+        when 'short' #input for address with only 1 street because dont want empty space again
+            address += "#{kind}: "
+            address += street_1
+            unless street_2.nil? || street_2.empty?
+                address += " " + street_2
+            end
+            address += ", #{city}, #{state}, #{postal_code}" 
+        end
+        address # prints the address as formatted above
+    end
 end
