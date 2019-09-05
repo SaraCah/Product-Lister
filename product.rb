dc8fe1e42b4c47af120cb81f0cab6878db0a2fc2
Sara Cave <saracaveg@gmail.com>
2019-09-05 16:57:13 +1000

    created method for add_rating_number. rating number calls upon previous class defined in RatingNumber. Than runs kind method on rating_number which gets the comment, than runs the number method to get the number rating. Than adds this value to empty array defined above

--- product.rb
+++ product.rb
@@ -11,3 +11,11 @@ class Product
         @addresses = []
     end
 
+    def add_rating_number(kind, number) #method for adding the comment and number out of 10
+        rating_number = RatingNumber.new #calls upon rating class
+        rating_number.kind = kind #gets the kind which is yet again meant to be comment, just havent changed it
+        rating_number.number = number #number for rating out for the 1-
+        rating_numbers << rating_number #than chucks it into the empty array
+    end
+
+
