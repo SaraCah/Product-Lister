172328046219bf7bc5f36fa5b2533259674be875
Sara Cave <saracaveg@gmail.com>
2019-09-05 17:14:35 +1000

    defined some methods to make the screen nice. Clear screen, move home and rest

--- product_book.rb
+++ product_book.rb
@@ -2,3 +2,25 @@ require './product'
 require 'yaml' #this is so i can write my data to yaml file
 
 class ProductBook
+
+    attr_reader :products
+
+    def initialize
+        @products = []  #want an empty array so user can put multiple products
+        open
+    end
+
+    # Clears the content on the screen.
+    def clear_screen!
+        print "\e[2J" 
+    end
+
+    # Moves the "cursor" back to the upper left.
+    def move_to_home!
+        print "\e[H"  # puts cursor on first line
+    end
+
+    def reset_screen! #method to rest screen
+        clear_screen!
+        move_to_home!
+    end
