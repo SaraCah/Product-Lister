d8a139a9fb686a613c7d62f3408778ad84d79b25
Sara Cave <saracaveg@gmail.com>
2019-09-05 17:17:43 +1000

    Made the loop for theuser menu interface. Gave options a p and s. When loop was broken it saves and resets the screen

--- product_book.rb
+++ product_book.rb
@@ -32,3 +32,41 @@ class ProductBook
     def save(file_name = 'products.yml') #method to save the products to yaml file
         File.open(file_name, 'w') { |file| file.write(products.to_yaml) }
     end
+
+    def run
+        loop do
+            reset_screen! #first reset screen and displays my menu
+            puts 'Products List Main Menu'.red
+            puts 'a: Add A Product'
+            puts 'p: Print Product List'
+            puts 's: Search Product List'
+            puts 'e: Save & Exit'
+            print 'Enter your choice: '
+
+            input = gets.chomp.downcase #prompts user to put in the option for above
+
+            case input 
+            when 'a'
+                add_product #returns to add_product method which defined below
+            when 'p'
+                print_label_list #prints all product list which is defined below
+            when 's'
+                reset_screen!  #resets me screen
+                print 'Search for: '
+                search = gets.strip # user input
+                find_by_name(search) # find by name method defined below
+                find_by_address(search) #if doesnt find by name than goes by find by address
+                find_by_rating_number(search) #if doenst find my address than goes by rating, really want to put find by comment somewhere her
+
+                print "Press enter at any time to return to the Main Menu: "
+                gets
+            # when input!= "a" || "p" || "s"  I need to fix because brain not working
+            #     prints "Sorry I didn't get that. Please try again"
+            #     input
+            when 'e'
+                save #on exit that's when it saves to yaml file, maybe should split this up?
+                reset_screen! #rest my screent again
+                break #breaks the loop and the end 
+            end
+        end
+    end
