a89a0124c945c396656228ea714651479ba29b36
Sara Cave <saracaveg@gmail.com>
2019-09-05 16:54:03 +1000

    needed to initaize an empty array for rating numbers and addresses

--- product.rb
+++ product.rb
@@ -5,3 +5,9 @@ class Product
 
     attr_writer :label_name, :type_name, :company_name
     attr_reader :rating_numbers, :addresses
+   
+    def initialize
+        @rating_numbers = [] #want and empty array to add for above and below
+        @addresses = []
+    end
+
